18d7624e2eb3d64e849e0789a21b6f6a2e8e23aa
Rajat Saxena <saxena1975sanjeev@gmail.com>
2020-09-20 18:44:18 +0530

    initial commit

---
+++ master.js
@@ -0,0 +1,9 @@
+function sideOpen() {
+  document.getElementById("sideDiv").style.width = '60%';
+  document.getElementById("sideDiv").style.display = 'block';
+}
+
+function sideClose() {
+  document.getElementById("sideDiv").style.width = '0';
+  document.getElementById("sideDiv").style.display = 'none';
+}
